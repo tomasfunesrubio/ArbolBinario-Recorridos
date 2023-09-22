bc2ab8b52f78e9247e34de56b3f2e69ce298b3bc
unknown <tomasfunesrubio@gmail.com>
2023-09-22 19:05:03 -0300

    Sixth commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Arbol_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("null");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("D");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("B", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("E");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("F");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("C", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("A", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode6});
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Recorrido en PreOrden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(386, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(43, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(326, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Recorrido en PreOrden";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(386, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(355, 221);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(386, 357);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 38);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(43, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(326, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Recorrido en InOrden";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(386, 420);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(355, 38);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(43, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(326, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Recorrido en PosOrden";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(386, 483);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(355, 38);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(43, 478);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(326, 46);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Recorrido en Amplitud";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(771, 41);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Nodo3";
+            treeNode1.Text = "null";
+            treeNode2.Name = "Nodo4";
+            treeNode2.Text = "D";
+            treeNode3.Name = "Nodo1";
+            treeNode3.Text = "B";
+            treeNode4.Name = "Nodo5";
+            treeNode4.Text = "E";
+            treeNode5.Name = "Nodo6";
+            treeNode5.Text = "F";
+            treeNode6.Name = "Nodo2";
+            treeNode6.Text = "C";
+            treeNode7.Name = "Nodo0";
+            treeNode7.Text = "A";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode7});
+            this.treeView1.Size = new System.Drawing.Size(295, 464);
+            this.treeView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 558);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbol_01
+{
+    public partial class Form1 : Form
+    {
+        Nodo nA, nB, nC, nD, nE, nF;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach(Nodo n in PreOrden())
+            {
+                listBox1.Items.Add(n.Id);
+            }
+        }
+        public List<Nodo> PreOrden()
+        {
+            List<Nodo> pListaNodos = new List<Nodo>();
+            PreOrdenRecursiva2(nA,pListaNodos);
+            return pListaNodos;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Clear();
+                InOrdenRecursiva(nA, textBox2);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void InOrdenRecursiva(Nodo pNodo, TextBox pCajaTexto)
+        {
+            if (pNodo != null)
+            {
+                InOrdenRecursiva(pNodo.Izquierdo, pCajaTexto);
+                pCajaTexto.Text += $"{pNodo.Id} ";
+                InOrdenRecursiva(pNodo.Derecho, pCajaTexto);
+            }
+        }
+        private void PosOrdenRecursiva(Nodo pNodo, TextBox pCajaTexto)
+        {
+            if (pNodo != null)
+            {
+                PosOrdenRecursiva(pNodo.Izquierdo, pCajaTexto);
+                PosOrdenRecursiva(pNodo.Derecho, pCajaTexto);
+                pCajaTexto.Text += $"{pNodo.Id} ";
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox3.Clear();
+                PosOrdenRecursiva(nA, textBox3);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            AmplitudRecursiva(new List<Nodo> {nA},textBox4);
+        }
+        private void AmplitudRecursiva(List<Nodo> pListaNodos, TextBox pCajaTexto)
+        {
+           if(pListaNodos.Exists(x => x.Id!="@"))
+            {
+                List<Nodo> auxListaNodos = new List<Nodo>();
+                foreach(Nodo n in pListaNodos)
+                {
+                    pCajaTexto.Text += $"{n.Id}";               
+                    auxListaNodos.Add(n.Izquierdo != null ? n.Izquierdo : new Nodo("@"));
+                    auxListaNodos.Add(n.Derecho != null ? n.Derecho : new Nodo("@"));
+                }
+                AmplitudRecursiva(auxListaNodos, pCajaTexto);
+               
+            }
+        }
+        private void PreOrdenRecursiva2(Nodo pNodo, List<Nodo> pListaNodos)
+        {
+            if (pNodo != null)
+            {
+                pListaNodos.Add(new Nodo(pNodo.Id));
+                PreOrdenRecursiva2(pNodo.Izquierdo, pListaNodos);
+                PreOrdenRecursiva2(pNodo.Derecho, pListaNodos);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                textBox1.Clear();
+                PreOrdenRecursiva(nA,textBox1);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+        private void PreOrdenRecursiva(Nodo pNodo, TextBox pCajaTexto)
+        {
+            if(pNodo!=null)
+            {
+                pCajaTexto.Text += $"{pNodo.Id} ";
+                PreOrdenRecursiva(pNodo.Izquierdo, pCajaTexto);
+                PreOrdenRecursiva(pNodo.Derecho, pCajaTexto);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nF = new Nodo("F");
+            nE = new Nodo("E");
+            nD = new Nodo("D");
+            nC = new Nodo("C",nE,nF);
+            nB = new Nodo("B",null, nD);
+            nA = new Nodo("A", nB, nC);
+
+            treeView1.ExpandAll();
+
+        }
+    }
+    public class Nodo
+    {
+        public Nodo(string pId="",Nodo pIzq=null,Nodo pDer=null) { Id = pId;Izquierdo = pIzq;Derecho = pDer; }
+        public string Id { get; set; }
+        public Nodo Izquierdo { get; set; }
+        public Nodo Derecho { get; set; }
+    }
+}
